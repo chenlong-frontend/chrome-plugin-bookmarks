0c2de1d89b5591d82bb713d6c96058698575a848
1016482011 <2956957982@qq.com>
2019-07-04 16:46:50 +0800

    添加登录

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,47 @@
 $(function() {
-  $('.get-url').on('click', function() {
-    // chrome.bookmarks.create({
-    //   parentId: '1',
-    //   title: '扩展程序文档',
-    //   url: 'https://developer.chrome.com/extensions'
+  // $('.get-url').on('click', function() {
+  //   // chrome.bookmarks.create({
+  //   //   parentId: '1',
+  //   //   title: '扩展程序文档',
+  //   //   url: 'https://developer.chrome.com/extensions'
+  //   // })
+  //   //
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  //     $('.show-url').text(tabs[0].url)
+  //   })
+  // })
+  // $('.set-data').on('click', function() {
+  //   chrome.storage.sync.set({ token: 'token' }, function() {
+  //     console.log('值保存好了')
+  //   })
+  // })
+  // $('.get-data').on('click', function() {
+  //   chrome.storage.sync.get('token', function(v) {
+  //     console.log(v)
+  //     $('.show-data').text(v.token)
+  //   })
+  // })
+  $('.login-submit').on('click', function(event) {
+    event.preventDefault()
+    var valueArr = $('.login-form').serializeArray()
+    var value = {}
+    valueArr.forEach(item => {
+      value[item.name] = item.value
+    })
+
+    // 向后台发起请求
+    // $.ajax({
+    //   method: 'POST',
+    //   url: 'xxx',
+    //   data: value
+    // }).done(function(msg) {
+    //   chrome.storage.sync.set({ token: 'token' }, function() {
+    //     console.log('值保存好了')
+    //   })
     // })
-    //
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      $('.show-url').text(tabs[0].url)
-    })
-  })
-
-  $('.set-data').on('click', function() {
-    chrome.storage.sync.set({ token: 'token' }, function() {
-      console.log('值保存好了')
-    })
-  })
-  $('.get-data').on('click', function() {
-    chrome.storage.sync.get('token', function(v) {
-      console.log(v)
-      $('.show-data').text(v.token)
-    })
+    // 请求结束切换显示
+    $('.login').hide()
+    $('.bookmark').show()
   })
 })
