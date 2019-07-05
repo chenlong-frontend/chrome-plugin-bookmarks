49f03ee6313a2a555c5b16e71fb2e2e64d8860b5
1016482011 <2956957982@qq.com>
2019-07-05 11:08:27 +0800

    添加注销按钮

--- js/popup.js
+++ js/popup.js
@@ -7,28 +7,7 @@ $(function() {
       $('.bookmark').show()
     }
   })
-  // $('.get-url').on('click', function() {
-  //   // chrome.bookmarks.create({
-  //   //   parentId: '1',
-  //   //   title: '扩展程序文档',
-  //   //   url: 'https://developer.chrome.com/extensions'
-  //   // })
-  //   //
-  //   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  //     $('.show-url').text(tabs[0].url)
-  //   })
-  // })
-  // $('.set-data').on('click', function() {
-  //   chrome.storage.sync.set({ token: 'token' }, function() {
-  //     console.log('值保存好了')
-  //   })
-  // })
-  // $('.get-data').on('click', function() {
-  //   chrome.storage.sync.get('token', function(v) {
-  //     console.log(v)
-  //     $('.show-data').text(v.token)
-  //   })
-  // })
+
   $('.login-submit').on('click', function(event) {
     event.preventDefault()
     var valueArr = $('.login-form').serializeArray()
@@ -38,18 +17,24 @@ $(function() {
     })
 
     // 向后台发起请求
-     $.ajax({
-       method: 'POST',
-       url: 'http://127.0.0.1:5000/auth/verifyUserInfo',
-       data: value
-     }).done(function(msg) {
-       chrome.storage.sync.set({ token: 'token' }, function() {
-         console.log('值保存好了')
-       })
-     })
+    $.ajax({
+      method: 'POST',
+      url: 'http://119.3.107.239:5000/auth/verifyUserInfo',
+      data: value
+    }).done(function(res) {
+      console.log(res)
+      // chrome.storage.sync.set({ token: 'token' }, function() {
+      //   // 请求结束切换显示
+      //   $('.login').hide()
+      //   $('.bookmark').show()
+      // })
+    })
+  })
 
-    // 请求结束切换显示
-    $('.login').hide()
-    $('.bookmark').show()
+  $('.logout').on('click', function() {
+    chrome.storage.sync.remove('token', function() {
+      $('.login').show()
+      $('.bookmark').hide()
+    })
   })
 })
